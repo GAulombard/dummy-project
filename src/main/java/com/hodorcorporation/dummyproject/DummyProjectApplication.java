75aa618a5406e335499ca3ed521c5b74a355887b
Hodor <geoffrey.aulombard@gmail.com>
2023-01-07 22:47:52 +0100

    first commit

---
+++ src/main/java/com/hodorcorporation/dummyproject/DummyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hodorcorporation.dummyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DummyProjectApplication.class, args);
+    }
+
+}
