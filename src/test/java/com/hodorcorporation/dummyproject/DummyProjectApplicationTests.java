0ec40807f6fda2058a175771000d1f5225e92a64
Hodor <geoffrey.aulombard@gmail.com>
2023-01-11 22:44:15 +0100

    working on GitHub actions

--- src/test/java/com/hodorcorporation/dummyproject/DummyProjectApplicationTests.java
+++ src/test/java/com/hodorcorporation/dummyproject/DummyProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.hodorcorporation.dummyproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DummyProjectApplicationTests.class)
 class DummyProjectApplicationTests {
 
     @Test
