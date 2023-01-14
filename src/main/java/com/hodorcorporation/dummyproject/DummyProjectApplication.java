5ed26b0cd953d21b3667c09d529833d3e3c05799
Hodor <geoffrey.aulombard@gmail.com>
2023-01-14 15:23:37 +0100

    working on workflow

--- src/main/java/com/hodorcorporation/dummyproject/DummyProjectApplication.java
+++ src/main/java/com/hodorcorporation/dummyproject/DummyProjectApplication.java
@@ -1,15 +1,7 @@
 package com.hodorcorporation.dummyproject;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.repository.query.Param;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import static org.springframework.http.ResponseEntity.status;
 
 @SpringBootApplication
 
