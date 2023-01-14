7006414e42e32d28e37aee7a21c49feae1e431a4
Hodor <geoffrey.aulombard@gmail.com>
2023-01-14 15:52:20 +0100

    working on workflow

--- src/main/java/com/hodorcorporation/dummyproject/DummyProjectApplication.java
+++ src/main/java/com/hodorcorporation/dummyproject/DummyProjectApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class DummyProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DummyProjectApplication.class, args);
     }
-
 
 }
