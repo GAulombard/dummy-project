5ea75dc5a3896d205709995a00446bb48c845001
Hodor <geoffrey.aulombard@gmail.com>
2023-01-16 13:54:33 +0100

    working on readme

--- src/test/java/com/hodorcorporation/dummyproject/DummyProjectApplicationTests.java
+++ src/test/java/com/hodorcorporation/dummyproject/DummyProjectApplicationTests.java
@@ -3,13 +3,15 @@ package com.hodorcorporation.dummyproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest(classes = DummyProjectApplicationTests.class)
 class DummyProjectApplicationTests {
 
 
     @Test
     void contextLoads() {
-
+        assertTrue(true);
     }
 
 }
